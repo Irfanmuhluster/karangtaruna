244306c13c03e54f5aaf439eb777a832f2ed1d6c
Irfan <muhirfan234@gmail.com>
2021-06-15 09:52:18 +0700

    blob

--- resources/views/admin/default/layout.blade.php
+++ resources/views/admin/default/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="Content-Security-Policy" content="img-src 'self' data:; default-src 'self' {{ url('/') }}">
+  <meta http-equiv="Content-Security-Policy" content="img-src 'self' {{ url('/') }};">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
   <title>AdminLTE 3 | Dashboard 3</title>
