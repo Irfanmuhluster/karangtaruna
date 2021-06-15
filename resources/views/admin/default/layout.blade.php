2f5e9f5ce407949a63acfcde8b5dc8753b56ebab
Irfan <muhirfan234@gmail.com>
2021-06-15 09:31:38 +0700

    sldmlsad

--- resources/views/admin/default/layout.blade.php
+++ resources/views/admin/default/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Security-Policy" content="font-src 'self' data:; img-src 'self' data:; default-src 'self' {{ url('/') }}">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
   <title>AdminLTE 3 | Dashboard 3</title>
@@ -10,7 +11,7 @@
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{ theme_asset('backend::default', 'css/fontawesome/css/all.min.css') }}">
   <!-- IonIcons -->
-  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ theme_asset('backend::default', 'css/bootstrap-4.min.css') }}">
   <link rel="stylesheet" href="{{ theme_asset('backend::default', 'css/adminlte.min.css') }}">
